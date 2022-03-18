0229c2e07b6c2f913fdec06d16493df0986e431e
Rob Grant <rob.grant@sensynehealth.com>
2022-03-18 20:48:57 +0000

    Trying out modules

--- bamboohr/plugin.go
+++ bamboohr/plugin.go
@@ -1,26 +1,19 @@
 package bamboohr
 
 import (
-    "context"
+	"context"
 
-    "github.com/turbot/steampipe-plugin-sdk/plugin"
-    "github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
 )
 
 func Plugin(ctx context.Context) *plugin.Plugin {
-    p := &plugin.Plugin{
-        Name:             "steampipe-plugin-bamboohr",
-        DefaultTransform: transform.FromGo().NullIfZero(),
-        TableMap: map[string]*plugin.Table{
-            "bamboohr_employees":        	tableBambooEmployee(),
-            // "zendesk_group":       	 		tableZendeskGroup(),
-            // "zendesk_organization":	 		tableZendeskOrganization(),
-            // "zendesk_search":      	 		tableZendeskSearch(),
-            // "zendesk_ticket":      	 		tableZendeskTicket(),
-            // "zendesk_ticket_audit":	 		tableZendeskTicketAudit(),
-            // "zendesk_trigger":     	 		tableZendeskTrigger(),
-            // "zendesk_user":         		tableZendeskUser(),
-        },
-    }
-    return p
+	p := &plugin.Plugin{
+		Name:             "steampipe-plugin-bamboohr",
+		DefaultTransform: transform.FromGo().NullIfZero(),
+		TableMap: map[string]*plugin.Table{
+			"bamboohr_employees": tableBambooEmployee(),
+		},
+	}
+	return p
 }
--- bamboohr/table_bamboohr_employees.go
+++ bamboohr/table_bamboohr_employees.go
@@ -1,43 +1,43 @@
 package bamboohr
 
 import (
-    "context"
+	"context"
 
-    "github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-    "github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
 
-    "github.com/robertlagrant/bamboohr-client-go"
+	bamboohr_client "github.com/robertlagrant/bamboohr-client-go"
 )
 
 func tableBambooEmployee() *plugin.Table {
-    return &plugin.Table {
-        Name:        "bamboohr_employee",
-        Description: "BambooHR only returns employees your credentials have access to.",
-        List: &plugin.ListConfig {
-            Hydrate: listEmployee,
-        },
-        // Get: &plugin.GetConfig {
-        //     KeyColumns: plugin.SingleColumn("id"),
-        //     Hydrate:    getUser,
-        // },
-        Columns: []*plugin.Column {
-            { Name: "firstName", Type: proto.ColumnType_STRING, Description: "Employee's first name" },
-            { Name: "lastName", Type: proto.ColumnType_STRING, Description: "Employee's last name" },
-            { Name: "displayName", Type: proto.ColumnType_STRING, Description: "Employee's full name for display" },
-            { Name: "jobTitle", Type: proto.ColumnType_STRING, Description: "Employee's job title" },
-            { Name: "department", Type: proto.ColumnType_STRING, Description: "Employee's department" },
-            { Name: "location", Type: proto.ColumnType_STRING, Description: "Employee's work location" },
-            { Name: "supervisor", Type: proto.ColumnType_STRING, Description: "Employee's supervisor" },
-        },
-    }
+	return &plugin.Table{
+		Name:        "bamboohr_employee",
+		Description: "BambooHR only returns employees your credentials have access to.",
+		List: &plugin.ListConfig{
+			Hydrate: listEmployee,
+		},
+		// Get: &plugin.GetConfig {
+		//     KeyColumns: plugin.SingleColumn("id"),
+		//     Hydrate:    getUser,
+		// },
+		Columns: []*plugin.Column{
+			{Name: "firstName", Type: proto.ColumnType_STRING, Description: "Employee's first name"},
+			{Name: "lastName", Type: proto.ColumnType_STRING, Description: "Employee's last name"},
+			{Name: "displayName", Type: proto.ColumnType_STRING, Description: "Employee's full name for display"},
+			{Name: "jobTitle", Type: proto.ColumnType_STRING, Description: "Employee's job title"},
+			{Name: "department", Type: proto.ColumnType_STRING, Description: "Employee's department"},
+			{Name: "location", Type: proto.ColumnType_STRING, Description: "Employee's work location"},
+			{Name: "supervisor", Type: proto.ColumnType_STRING, Description: "Employee's supervisor"},
+		},
+	}
 }
 
 func listEmployee(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-    employees := bamboohr_client.ListEmployees()
-    for _, employee := range employees {
+	employees, _ := bamboohr_client.ListEmployees()
+	for _, employee := range employees {
 
-        d.StreamListItem(ctx, employee)
-    }
+		d.StreamListItem(ctx, employee)
+	}
 
-    return nil, nil
+	return nil, nil
 }
